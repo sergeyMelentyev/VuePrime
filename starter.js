81ff5f3e1e2584306d14a2071c88b3a7ea3df98c
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-04-21 17:05:26 +0300

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,11 +1,30 @@
 (function () {
     "use strict";
     var data = {
-        message: "Received from server."
+        message: "Received from server.",
+        arr: ["One", "Two"],
+        arrOfObjs: [{
+            writer: "value one",
+            plot: "Data one"
+        },{
+            writer: "value two",
+            plot: "Data two"
+        }],
+        obj: {
+            keyOne: "value1",
+            keyTwo: "value2"
+        },
+        votes: 0
     };
+    function upVoteMethod() {
+        this.votes++;
+    }
     new Vue({
         el: "#app",
-        data: data
+        data: data,
+        methods: {
+            upVote: upVoteMethod
+        }
     });
 
 })();
