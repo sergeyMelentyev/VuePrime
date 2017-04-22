0bda6b702b2574a15d2c93604c67b2af2184cc67
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-04-22 18:08:55 +0300

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,6 +1,7 @@
 (function () {
     "use strict";
-    var data = {
+
+    var firstDataSet = {
         message: "Received from server.",
         arr: ["One", "Two"],
         arrOfObjs: [{
@@ -16,14 +17,69 @@
         },
         votes: 0
     };
+    var secondDataSet = {
+        a: null,
+        b: null,
+        operator: "+",
+        c: null
+    };
+    var thirdDataSet = {
+        candidates: [
+            {name: "Mr. Black", votes: 0},
+            {name: "Mr. White", votes: 0},
+            {name: "Mr. Green", votes: 0},
+            {name: "Mr. Red", votes: 0}]
+    };
+
     function upVoteMethod() {
-        this.votes++;
+        this.firstDataSet.votes++;
     }
+    function calc() {
+        switch (this.secondDataSet.operator) {
+            case "+":
+                this.secondDataSet.c = this.secondDataSet.a + this.secondDataSet.b;
+                return;
+            case "-":
+                this.secondDataSet.c = this.secondDataSet.a - this.secondDataSet.b;
+                return;
+            case "*":
+                this.secondDataSet.c = this.secondDataSet.a * this.secondDataSet.b;
+                return;
+            case "/":
+                this.secondDataSet.c = this.secondDataSet.a / this.secondDataSet.b;
+                return;
+        }
+    }
+    function mayorVote(candidate) {
+        return candidate.votes++;
+    }
+    function clearResults() {
+        this.thirdDataSet.candidates = this.thirdDataSet.candidates.map(function (candidate) {
+            candidate.votes = 0;
+            return candidate;
+        })
+    }
+
     new Vue({
         el: "#app",
-        data: data,
+        data: {
+            firstDataSet: firstDataSet,
+            secondDataSet: secondDataSet,
+            thirdDataSet: thirdDataSet
+        },
         methods: {
-            upVote: upVoteMethod
+            upVote: upVoteMethod,
+            calc: calc,
+            mayorVote: mayorVote,
+            clearResults: clearResults
+        },
+        computed: {
+            mayor: function () {
+                var candidatesSorted = this.thirdDataSet.candidates.sort(function (a, b) {
+                    return b.votes - a.votes;
+                });
+                return candidatesSorted[0];
+            }
         }
     });
 
