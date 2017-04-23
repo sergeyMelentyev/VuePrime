18d36ab0bd31307e49f9e5352e4b29192b44e261
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-04-23 16:30:13 +0300

    Update starter.js

--- starter.js
+++ starter.js
@@ -30,6 +30,48 @@
             {name: "Mr. Green", votes: 0},
             {name: "Mr. Red", votes: 0}]
     };
+    var fourthDataSet = {
+        query: "",
+        stories: [
+            {
+                plot: "First story",
+                writer: "First writer",
+                upvotes: 2,
+                voted: false
+            },
+            {
+                plot: "Second story",
+                writer: "Second writer",
+                upvotes: 8,
+                voted: false
+            },
+            {
+                plot: "Third story",
+                writer: "First writer",
+                upvotes: 5,
+                voted: false
+            },
+            {
+                plot: "Fourth story",
+                writer: "Second writer",
+                upvotes: 74,
+                voted: false
+            }],
+        favorite: {},
+        heroes: [
+            {
+                name: "First Name", last: "First Last Name", secretId: "First ID"
+            },
+            {
+                name: "Second Name", last: "Second Last Name", secretId: "Second ID"
+            },
+            {
+                name: "Third Name", last: "Third Last Name", secretId: "Third ID"
+            },
+            {
+                name: "Fourth Name", last: "Fourth Last Name", secretId: "Fourth ID"
+            }]
+    };
 
     function upVoteMethod() {
         this.firstDataSet.votes++;
@@ -59,19 +101,51 @@
             return candidate;
         })
     }
+    function storiesBy(writer) {
+        return this.fourthDataSet.stories.filter(function (story) {
+            return story.writer === writer;
+        });
+    }
+
+    Vue.filter('snitch', function (hero) {
+        return hero.secretId + ' is '
+            + hero.name + ' '
+            + hero.last;
+    });
+
+    Vue.component('story', {
+        template: "#story-template",
+        props: ['item'],
+        methods: {
+            upvote: function () {
+                this.item.upvotes += 1;
+                this.item.voted = true;
+            },
+            setFavorite: function () {
+                fourthDataSet.favorite = this.item;
+            }
+        },
+        computed: {
+            isFavorite: function () {
+                return this.item == fourthDataSet.favorite;
+            }
+        }
+    });
 
     new Vue({
         el: "#app",
         data: {
             firstDataSet: firstDataSet,
             secondDataSet: secondDataSet,
-            thirdDataSet: thirdDataSet
+            thirdDataSet: thirdDataSet,
+            fourthDataSet: fourthDataSet
         },
         methods: {
             upVote: upVoteMethod,
             calc: calc,
             mayorVote: mayorVote,
-            clearResults: clearResults
+            clearResults: clearResults,
+            storiesBy: storiesBy
         },
         computed: {
             mayor: function () {
@@ -79,6 +153,17 @@
                     return b.votes - a.votes;
                 });
                 return candidatesSorted[0];
+            },
+            famous: function () {
+                return this.fourthDataSet.stories.filter(function (item) {
+                    return item.upvotes > 25;
+                });
+            },
+            search: function () {
+                var query = this.fourthDataSet.query;
+                return this.fourthDataSet.stories.filter(function (item) {
+                    return item.plot.includes(query);
+                });
             }
         }
     });
