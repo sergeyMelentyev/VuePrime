b379ec106ef91f713aa5ab11503eb2d0d640c9d6
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-04-24 16:02:47 +0300

    Update events.js

--- events.js
+++ events.js
@@ -1,6 +1,8 @@
 (function () {
     "use strict";
 
+    var bus = new Vue();
+
     Vue.component('food', {
         template: '#food',
         props: ['name'],
@@ -11,23 +13,41 @@
         },
         methods: {
             vote: function (event) {
+                var food = event.srcElement.textContent;
                 this.votes++;
-                this.$emit('voted', event.srcElement.textContent);
+                bus.$emit('voted', food);
+            },
+            reset: function () {
+                this.votes = 0;
             }
+        },
+        created: function () {
+            bus.$on('reset', this.reset);
         }
     });
 
     new Vue({
         el: '#app2',
         data: {
-            votes: 0,
-            log: []
+            votes: {
+                count: 0,
+                log: []
+            }
         },
         methods: {
-            countVote: function (data) {
-                this.votes++;
-                this.log.push(data);
+            countVote: function (food) {
+                this.votes.count++;
+                this.votes.log.push(food);
+            },
+            reset: function () {
+                this.votes = {
+                    count: 0,
+                    log: []
+                };
             }
+        },
+        created: function () {
+            bus.$on('voted', this.countVote);
         }
     });
 })();
