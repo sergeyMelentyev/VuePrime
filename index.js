cf41d031a4deef9996c7335e8e8f76e7d4f1b38d
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-05-04 12:35:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@
     // push, pop, shift, unshift, splice, sort, reverse.	mutation methods
     // filter, concat, slice.			// not mutation methods, return a new array
 
-	// <li v-for="n in evenNumbers">{{ n }}</li>
+	// <li v-for="n in even">{{ n }}</li>
 	export default {
         data: function () {
             return {
@@ -147,6 +147,138 @@
         }
     }
 })(window);
+
+(function(events){
+	// <a v-on:click.stop.prevent="doThat"></a>		// event modifiers .capture .self .once
+	// <input v-on:keyup.13="submit">		// only call vm.submit() when the keyCode is 13
+	// <button v-on:click="send('+', $event)"></button>		// access the original DOM event
+	export default {
+        data: function () {
+            return {
+                name: ''
+            };
+        },
+        methods: {
+            send: function (obj, event) {
+                this.name = obj;
+                console.log(this.name + ' ' + event.type);
+            }
+        }
+    }
+
+    // Parent-Child Communication
+    // v-on must use to listen to events emitted by children
+    // <div> Total is {{total}} </div>
+    // <button-counter v-on:eventNameFromChild="parentMethodName"></button-counter>
+    Vue.component('button-counter', {
+        data: function () {
+            return {
+                counter: 0
+            };
+        },
+        template: '<button v-on:click="childMethodName"> {{counter}} </button>',
+        methods: {
+            childMethodName: function () {
+                ++this.counter;
+                this.$emit('eventNameFromChild');
+            }
+        }
+    });
+
+    export default {
+        name: 'app',
+        data: function () {
+            return {
+                total: 0
+            };
+        },
+        methods: {
+            parentMethodName: function () {
+                ++this.total;
+            }
+        }
+    }
+
+    // Non Parent-Child Communication
+    var bus = new Vue();
+	bus.$emit('id-selected', 1)		// first component method
+	bus.$on('id-selected', function (id) { });	// second component created hook
+})(window);
+
+(function(forms){
+	// <input v-bind:value="something" v-on:input="something = $event.target.value">
+	// <input v-model="something">			// the same as above
+	
+	/*<select v-model="selected">
+		<option v-for="option in options" v-bind:value="option.value">
+			{{ option.text }}
+		</option>
+	</select>
+	<span>Selected: {{ selected }}</span>*/
+	
+	export default {
+        data: function () {
+            return {
+                selected: 'A',
+                options: [
+                    { text: 'One', value: 'A' },
+                    { text: 'Two', value: 'B' },
+                    { text: 'Three', value: 'C' }
+                ]
+            };
+        }
+    }
+})(window);
+
+(function(components){
+	// <custom> simple component with inline template </custom>
+    Vue.component('custom', {
+        template: '<button v-on:click="addOne"> {{counter}} </button>',
+        methods: {
+            addOne: function () {
+                ++this.counter;
+            }
+        },
+        data: function () {
+            return {
+                counter: 0
+            }
+        }
+    });
+    export default {
+        name: 'app'
+    }
+
+    // Dynamic Props
+    // v-bind dynamically bind child props to parent data
+    
+    // <input v-model="parent">
+    // <child v-bind:childProp="parent"></child>
+    Vue.component('child', {
+        props: ['childProp'],
+        computed: {
+            finalVal: function () {
+                return this.childProp.toUpperCase();
+            }
+        },
+        template: '<p> Data from parent: {{finalVal}} </p>'
+    });
+    export default {
+        name: 'app',
+        data: function () {
+            return {
+                parent: ''
+            };
+        }
+    }
+})(window);
+
+(function(slots){
+	
+})(window);
+
+
+
 
 
 
